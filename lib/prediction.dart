0fc879b1881fbe5c8dec07bd2fc6807b901c324a
Yuktika089 <95289306+Yuktika089@users.noreply.github.com>
2023-11-14 23:04:34 +0530

    Create prediction.dart

---
+++ lib/prediction.dart
@@ -0,0 +1,180 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:rises/constants.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  var child;
+  var children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'RISE - Heal and Rise!',
+          style: TextStyle(color: Color(0xfffcfcfc)),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Add functionality for the back button
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[],
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: PRIMARY_DARK_PINK,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Your Mood Chart',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Your rise is unstoppable! Keep up the positive spirit.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 16),
+                    CircularProgressIndicator(
+                      value: 0.8, // Adjust the value between 0.0 and 1.0
+                      color: Colors.white,
+                      backgroundColor: Color(0xff0d0b0b),
+                      strokeWidth: 10,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '80%',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xff4fa8f0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Your Music Taste',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Your beats make us happy! Keep vibing to better tunes.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 16),
+                    CircularProgressIndicator(
+                      value: 0.6, // Adjust the value between 0.0 and 1.0
+                      color: Colors.white,
+                      backgroundColor: Color(0xff110f0f),
+                      strokeWidth: 10,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '60%',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: PRIMARY_DARK_PINK,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Your Regularity in Entries',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Proud of you to journal your events! Keep writing about your days.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 16),
+                    CircularProgressIndicator(
+                      value: 1.0, // Adjust the value between 0.0 and 1.0
+                      color: Colors.white,
+                      backgroundColor: Color(0xff121111),
+                      strokeWidth: 10,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '100%',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
