633b6d596e7eb2695bffce27a89564cdd2b79b79
manasvvi1 <manasvvi.aggarwal@gmail.com>
2023-11-16 20:14:42 +0530

    analytics code pasted

--- lib/Analytics.dart
+++ lib/Analytics.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:rise/constants.dart';
+import 'package:flutter/material.dart';
 
 class Analytics extends StatefulWidget {
   const Analytics({Key? key}) : super(key: key);
@@ -10,6 +12,153 @@ class Analytics extends StatefulWidget {
 class _AnalyticsState extends State<Analytics> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            ),
+            SizedBox(height: 16),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: PRIMARY_DARK_PINK,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Your Mood Chart',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Your rise is unstoppable! Keep up the positive spirit.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  CircularProgressIndicator(
+                    value: 0.8, // Adjust the value between 0.0 and 1.0
+                    color: Colors.white,
+                    backgroundColor: Color(0xff0d0b0b),
+                    strokeWidth: 10,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '80%',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color(0xff4fa8f0),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Your Music Taste',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Your beats make us happy! Keep vibing to better tunes.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  CircularProgressIndicator(
+                    value: 0.6, // Adjust the value between 0.0 and 1.0
+                    color: Colors.white,
+                    backgroundColor: Color(0xff110f0f),
+                    strokeWidth: 10,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '60%',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: PRIMARY_DARK_PINK,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Your Regularity in Entries',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Proud of you to journal your events! Keep writing about your days.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  CircularProgressIndicator(
+                    value: 1.0, // Adjust the value between 0.0 and 1.0
+                    color: Colors.white,
+                    backgroundColor: Color(0xff121111),
+                    strokeWidth: 10,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '100%',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
